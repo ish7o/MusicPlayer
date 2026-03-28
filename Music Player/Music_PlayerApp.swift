88b5297fb34b34738ba90b93724d3d419a9bac35
Julian Orlowski <julian.sz.orlowski@gmail.com>
2026-03-28 16:37:09 +0100

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by Julian Orłowski on 28/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Music PlayerTests/Music_PlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerTests.swift
+//  Music PlayerTests
+//
+//  Created by Julian Orłowski on 28/03/2026.
+//
+
+import Testing
+@testable import Music_Player
+
+struct Music_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
